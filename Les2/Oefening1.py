8a6df7a747380a1e5575f6cd34f2ca5a7e2b66a4
unknown <r0913415@student.thomasmore.be>
2023-10-11 20:47:49 +0200

    Oefeningen

--- Les2/Oefening1.py
+++ Les2/Oefening1.py
@@ -3,4 +3,4 @@ getal = int(input("Geef een getal: "))
 if getal % 3 == 0:
     print(getal, "is een drievoud.")
 else:
-    print(getal, "is niet deelbaar door 3.")
+    print(getal, "is niet deelbaar door 3.") 
