0a0a2740f4bd1a652eec18c99599bbed4fe6679c
jimmydehTM <r0913415@student.thomasmore.be>
2023-10-24 16:58:19 +0200

    Oefeningen

--- Les4/Oefening7.py
+++ Les4/Oefening7.py
@@ -1,17 +1,12 @@
 lijst = [0, 45, 60, 13, 0, 42, 18, 17, 0, 26, 13]
-b = []
-i = 0
 
-
-while i < len(lijst):
-    for i in len(lijst):
-        if lijst[i] == 0:
-            z = lijst[i+1]
-            x = lijst.pop(lijst[i])
-            lijst.insert(x, z)
-        
-        
-        
-    i+=1
+for i in range(len(lijst) - 1):
+    if lijst[i] == 0:
+        x = i + 1
+        while x < len(lijst):
+            if lijst[x] % 2 != 0:
+                lijst[i] = lijst[x]
+                break
+            x += 1
 
 print(lijst)
